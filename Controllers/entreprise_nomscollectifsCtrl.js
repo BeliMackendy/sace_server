0b11c28293c8d98ce9f6621f39da2b8b4e9fb0df
BeliMackendy <belizaire.mackendy@gmail.com>
2020-06-04 15:41:34 -0700

    update ent_noms_coll

--- Controllers/entreprise_nomscollectifsCtrl.js
+++ Controllers/entreprise_nomscollectifsCtrl.js
@@ -1,68 +1,61 @@
 const entreprise_nomscollectifsDb = require("../Models/entreprise_nomscollectifsDb");
-var fs =require('fs')
+var fs = require("fs");
+const path = require("path");
+
+const save_image = (id, image) => {
+  fs.mkdirSync(`./uploads/${id}`, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  image.mv(`./uploads/${id}/${image.name}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+    // res.json({
+    //   filename: `${acte_constitutif.name}`,
+    //   filePath: `/uploads/${id}/${acte_constitutif.name}`,
+    // });
+  });
+};
 
 exports.Insert = async (req, res) => {
   const { id, denomination, nature } = req.body;
-  console.log(req.body)
-
+  // console.log(req.body);
   const { acte_constitutif, reconnaissance, copie_p_identite } = req.files;
-  fs.mkdirSync(`./uploads/${id}`)
+  // console.log(req.files);
 
-  acte_constitutif.mv(`./uploads/${id}/${acte_constitutif.name}`, (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(err);
-    }
-    res.json({
-      filename: `${acte_constitutif.name}`,
-      filePath: `/uploads/${id}/${acte_constitutif.name}`,
-    });
-  });
-
-  reconnaissance.mv(`./uploads/${id}/${reconnaissance.name}`, (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(err);
-    }
-    res.json({
-      filename: `${reconnaissance.name}`,
-      filePath: `/uploads/${id}/${reconnaissance.name}`,
-    });
-  });
   
-  copie_p_identite.mv(`./uploads/${id}/${copie_p_identite.name}`, (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send(err);
+
+  const acte_constitutif_path = `/uploads/${id}/${acte_constitutif.name}`;
+  const reconnaissance_path = `/uploads/${id}/${reconnaissance.name}`;
+  const copie_p_identite_path = `/uploads/${id}/${copie_p_identite.name}`;
+
+  const newentreprise = {
+    id_institution: id,
+    denomination: denomination,
+    nature: nature,
+    acte_constitutif: acte_constitutif_path,
+    reconnaissance: reconnaissance_path,
+    copie_p_identite: copie_p_identite_path,
+  };  
+
+  await entreprise_nomscollectifsDb.Insert(newentreprise, (err, result) => {
+    if (err) console.log(err);
+    else {
+      save_image(id, acte_constitutif);
+      save_image(id, reconnaissance);
+      save_image(id, copie_p_identite);
+      const newentreprise_ = {
+        id_institution: id,
+        denomination: denomination,
+        nature: nature,
+        acte_constitutif: acte_constitutif_path,
+        reconnaissance: reconnaissance_path,
+        copie_p_identite: copie_p_identite_path,
+      };
+
+      res.json(newentreprise_);
     }
-    res.json({
-      filename: `${copie_p_identite.name}`,
-      filePath: `/uploads/${id}/${copie_p_identite.name}`,
-    });
   });
-
-  // const newentreprise = {
-  //   id_institution: id_institution,
-  //   denomination: denomination,
-  //   nature: nature,
-  //   acte_constitutif: acte_constitutif,
-  //   reconnaissance: reconnaissance,
-  //   copie_p_identite: copie_p_identite,
-  // };
-
-  // await entreprise_nomscollectifsDb.Insert(newentreprise, (err, result) => {
-  //   if (err) console.log(err);
-  //   else {
-  //     const newentreprise_ = {
-  //       id_institution: id_institution,
-  //       denomination: denomination,
-  //       nature: nature,
-  //       acte_constitutif: acte_constitutif,
-  //       reconnaissance: reconnaissance,
-  //       copie_p_identite: copie_p_identite,
-  //     };
-
-  //     res.json(newentreprise_);
-  //   }
-  // });
 };
